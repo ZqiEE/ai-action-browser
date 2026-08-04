8eb79fc73af7ea8a58ea2f201ad504b4b64e4754
ZqiEE <18942619472@163.com>
2026-08-04 18:53:24 +0800

    Load enhanced component styles

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -4,6 +4,7 @@ import { App } from "@/App";
 import { getTextDirection, resolveLocale } from "@/lib/locale";
 import "@/styles/tokens.css";
 import "@/styles/global.css";
+import "@/styles/enhancements.css";
 
 const locale = resolveLocale();
 document.documentElement.lang = locale;
