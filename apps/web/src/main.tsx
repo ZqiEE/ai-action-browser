6aafd18ffc5e27f3ee932ea9befe4d5d39b1b301
ZqiEE <18942619472@163.com>
2026-08-06 22:27:08 +0800

    Load production flow styles

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -5,6 +5,7 @@ import { getTextDirection, resolveLocale } from "@/lib/locale";
 import "@/styles/tokens.css";
 import "@/styles/global.css";
 import "@/styles/enhancements.css";
+import "@/styles/production.css";
 
 const locale = resolveLocale();
 document.documentElement.lang = locale;
